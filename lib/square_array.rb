1835eb8c0df6873779cfeaf9e5d222ab4295f718
Sharnell Grant <sharnellgrant@gmail.com>
2019-11-24 01:22:11 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,10 +2,7 @@ def square_array(array)
 end
 
 def square_array(array)
-<<<<<<< HEAD
   numbers = [1, 2, 3]
-=======
->>>>>>> 1e329c0779cb77ad39e0c7a27c7c03f65c1d6308
 sum = Math.sqrt(numbers)
 
 counter = 0
