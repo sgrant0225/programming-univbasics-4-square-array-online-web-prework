59bea8c7d90cbab25b48cbe473ad951e3c61fce5
Sharnell Grant <sharnellgrant@gmail.com>
2019-11-24 01:35:32 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,11 +2,11 @@ def square_array(array)
 end
 
 
-array = [1, 2, 3]
+array.to_i = [1, 2, 3]
  
 counter = 0
 while counter[array] do
-  array[counter]{|num| num ** 2}.to_i
+  array[counter]{|num| num ** 2}
   counter += 1
 end
 
